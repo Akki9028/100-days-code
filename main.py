580429f2eb1a6e8cd2b4c357f0ce050aba59fe59
AkshayPatil90 <patilakshay239@gmail.com>
2025-11-18 20:35:17 +0530

    Test

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import requests
+# import requests
 
-MY_LAT = 20.593684
-MY_LNG = 78.96288
+# MY_LAT = 20.593684
+# MY_LNG = 78.96288
 
-parameters = {
-  "lat": MY_LAT,
-  "lng": MY_LNG,
-}
+# parameters = {
+#   "lat": MY_LAT,
+#   "lng": MY_LNG,
+# }
 
-response = requests.get("https://api.sunrise-sunset.org/json",
-                        params=parameters)
-response.raise_for_status()
-data = response.json()
-print(data)
+# response = requests.get("https://api.sunrise-sunset.org/json",
+#                         params=parameters)
+# response.raise_for_status()
+# data = response.json()
+# print(data)
+
+import matplotlib.pyplot as plt
+
+x = [24, 25, 26]
+y = [23, 24, 25]
+
+plt.plot(x,y)
+plt.show()
